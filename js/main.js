88080491ab7f7a47f92c5ec64ac456031e679986
Jessica <jesscara.993@gmail.com>
2022-11-11 18:05:17 +0100

    added js for items and next

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const items = document.getElementsByClassName("item");
+
+let activeItems = 0;
+
+// NAVIGAZIONE
+const next = document.querySelector(".next");
+
+next.addEventListener("click",
+function() {
+
+  if (activeItems < items.length - 1){
+    // RIMUOVERE CLASSE ACTIVE ALL'IMG SELEZIONATA
+    items[activeItems].classList.remove("active");
+
+    // INCREMENTO INDICE DELL'IMG DA VISUALIZZARE
+    activeItems ++;
+
+    // AGGIUNGERE CLASSE ACTIVE ALLA PROSSIMA IMG
+    items[activeItems].classList.add("active");
+  }
+
+  if (activeItems === items.length - 1){
+    // ALL'ULTIMA IMG DEL CAROUSEL RIMUOVERE FRECCIA
+    next.classList.add("hidden");
+  }
+
+}
+);
