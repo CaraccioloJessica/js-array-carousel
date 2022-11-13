546509209a676ef04b240fe01108591a3007cfb8
Jessica <jesscara.993@gmail.com>
2022-11-13 13:03:56 +0100

    carousel work, up btn not working after last img

--- js/main.js
+++ js/main.js
@@ -7,15 +7,49 @@ let images = [
   "img/05.webp", 
 ]
 
-let activeItems = 0;
+//INDEX ATTUALE
+let index = 0;
 
-// VARIABILI GENERALI
-let items = document. querySelector(".item");
-
-// NAVIGAZIONE
-const next = document.querySelector(".next");
-items.classList.add("active");
+// VARIABILE CONTAINER
+let itemContenitore = document.querySelector(".item-container");
 
 for (let i = 0; i < images.length; i++){
-  items.innerHTML+=`<img src="${images[i]}">`
- }
+  itemContenitore.innerHTML+=
+  `<div class="item" id="slide${i}">
+    <img src="${images[i]}">
+  </div>`
+}
+
+// IMMAGINI ATTIVE
+let imgVisualizzata = document.getElementById("slide" + index);
+imgVisualizzata.classList.add("active");
+
+// NAVIGAZIONE
+const up = document.querySelector(".up");
+const down = document.querySelector(".down");
+
+// BOTTONE GIU
+down.addEventListener("click",
+function (){
+  index--;
+  if (index < 0) {
+    index = images.length - 1;
+  }
+  imgVisualizzata.classList.remove("active");
+  imgVisualizzata = document.getElementById("slide" + index);
+  imgVisualizzata.classList.add("active");
+  }
+ );
+  
+//  BOTTONE SU
+up.addEventListener("click",
+ function () {
+  index++;
+  if (index > 4) {
+    index = images.length + 1;
+  }
+  imgVisualizzata.classList.remove("active");
+  imgVisualizzata = document.getElementById("slide" + index);
+  imgVisualizzata.classList.add("active");  
+  }
+ );
